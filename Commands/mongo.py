a414ee5c0eb6b9db733f6caed53d7eb821571f9f
elsie200 <72025040+elsie200@users.noreply.github.com>
2024-12-06 11:35:22 +0100

    Update mongo.py

--- Commands/mongo.py
+++ Commands/mongo.py
@@ -4,7 +4,7 @@ import certifi
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb+srv://Cluster60849:S2NxfHFnXmhP@cluster60849.p6ste.mongodb.net/"
+   CONNECTION_STRING = "your_connexion_string"
  
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING,tlsCAFile=certifi.where())
